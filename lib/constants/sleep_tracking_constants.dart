c42e8dfaff5d5ce54c6035cb20c35853d16a7ecf
Mateus Freitas <mateus.gomes.freitas@wunderdog.fi>
2025-01-25 16:30:44 +0100

    Add sleep details screen with chart for displaying categories of sleep. Add new chart.

---
+++ lib/constants/sleep_tracking_constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const int AWAKE_INDEX = 0;
+const int REM_INDEX = 1;
+const int LIGHT_SLEEP_INDEX = 2;
+const int DEEP_SLEEP_INDEX = 3;
+
+const Color AWAKE_COLOR = Colors.red;
+const Color REM_COLOR = Colors.grey;
+const Color LIGHT_SLEEP_COLOR = Colors.green;
+const Color DEEP_SLEEP_COLOR = Colors.blue;
